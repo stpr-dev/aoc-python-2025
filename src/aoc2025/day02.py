784970460df3dd3266e5284589f971ae24c56653
stpr-dev <15972594+stpr-dev@users.noreply.github.com>
2025-12-03 05:11:02 +0530

    Brute force method of finding part 2 answer. Verified.

--- src/aoc2025/day02.py
+++ src/aoc2025/day02.py
@@ -4,7 +4,7 @@ from pprint import pprint
 from utils.io import read_input
 
 
-def get_invalid_ids_brute_force(ranges: list[list[int]]) -> list[int]:
+def part1_get_invalid_ids_brute_force(ranges: list[list[int]]) -> list[int]:
     """Get invalid IDs as defined by Part 1 of the AoC challenge.
 
     Args:
@@ -59,6 +59,54 @@ def get_invalid_ids_brute_force(ranges: list[list[int]]) -> list[int]:
     return invalid_ids
 
 
+def factors(n: int) -> list[int]:
+    return [factor for factor in range(1, n + 1) if n % factor == 0]
+
+
+def part2_get_invalid_ids_brute_force(ranges: list[list[int]]) -> list[int]:
+    """Get invalid IDs as defined by Part 2 of the AoC challenge.
+
+    Args:
+        ranges: List of integer ranges, where each range is a list of two integers.
+
+    Returns:
+        List of invalid IDs.
+
+    Notes:
+        This approach uses brute force and is extremely slow. Only use for
+            validation purposes.
+    """
+
+    # Unlike part 1, this time it's not just HALF of the string, but any n-substring
+    # of the number that is repeated. So 123123123 is valid now, for instance.
+    # So looking at it from another lens, the question is essentially asking: if we
+    # have a string of length n, can we decompose it into smaller substrings and test
+    # for equality? From a mathematical perspective, it is essentially finding
+    # factors of the length of the original string and finding if repeating THAT
+    # substring gives us the original string.
+
+    invalid_ids: list[int] = []
+
+    for range_ in ranges:
+        lower_number: int = range_[0]
+
+        higher_number: int = range_[1]
+
+        # Now loop through the range.
+        for num in range(lower_number, higher_number + 1):
+            num_str: str = str(num)
+
+            fac: list[int] = factors(len(num_str))
+            _ = fac.pop()  # Remove the last element, which is the number itself
+
+            for factor in fac:
+                if num_str[:factor] * (len(num_str) // factor) == num_str:
+                    invalid_ids.append(num)
+                    break
+
+    return invalid_ids
+
+
 def main() -> None:
     data_path: Path = (
         Path(__file__).parent.parent.parent / "data" / "2025" / "day02.txt"
@@ -84,10 +132,14 @@ def main() -> None:
 
     pprint(ranges)
 
-    invalid_ids: list[int] = get_invalid_ids_brute_force(ranges)
+    invalid_ids: list[int] = part1_get_invalid_ids_brute_force(ranges)
     pprint(f"Invalid IDs: {invalid_ids}")
 
     pprint(f"Part 1 solution is: {sum(invalid_ids)}.")
+
+    invalid_ids = part2_get_invalid_ids_brute_force(ranges)
+    pprint(f"Invalid IDs: {invalid_ids}")
+    pprint(f"Part 2 solution is: {sum(invalid_ids)}.")
 
 
 if __name__ == "__main__":
