645d2aa2b8213b53d1a1a828d3944af91ad2d3bd
stpr-dev <15972594+stpr-dev@users.noreply.github.com>
2025-12-04 21:12:10 +0530

    - Added tests for day 2. - Update day 3 test to use parametrize instead of looping.

--- tests/test_day03.py
+++ tests/test_day03.py
@@ -1,38 +1,59 @@
 import aoc2025.day03 as d03
+import pytest
 
 
-def test_part1_examples():
-    data = [
-        "987654321111111",
-        "811111111111119",
-        "234234234234278",
-        "818181911112111",
-    ]
+@pytest.mark.parametrize(
+    "num,expected",
+    [
+        (["987654321111111"], [98]),
+        (["811111111111119"], [89]),
+        (["234234234234278"], [78]),
+        (["818181911112111"], [92]),
+    ],
+)
+def test_part1_examples(num, expected):
+    out = d03.part1_get_largest_2_digit(num)
 
-    out = d03.part1_get_largest_2_digit(data)
-
-    assert out == [98, 89, 78, 92]
-
-
-def test_get_largest_sub_list_basic():
-    assert d03.get_largest_sub_list(list("987654")) == list("98765")
-    assert d03.get_largest_sub_list(list("123456")) == list("23456")
-    assert d03.get_largest_sub_list(list("132456")) == list("32456")
+    assert out == expected
 
 
-def test_part2_examples():
-    data = [
-        "987654321111111",
-        "811111111111119",
-        "234234234234278",
-        "818181911112111",
-    ]
+@pytest.mark.parametrize(
+    "input_list,expected",
+    [
+        (list("987654"), list("98765")),
+        (list("123456"), list("23456")),
+        (list("132456"), list("32456")),
+    ],
+)
+def test_get_largest_sub_list_basic(input_list, expected):
+    assert d03.get_largest_sub_list(input_list) == expected
 
-    out = d03.part2_get_largest_k_digit(data, k=12)
 
-    assert out == [
-        987654321111,
-        811111111119,
-        434234234278,
-        888911112111,
-    ]
+@pytest.mark.parametrize(
+    "num,expected",
+    [
+        (["987654321111111"], [987654321111]),
+        (["811111111111119"], [811111111119]),
+        (["234234234234278"], [434234234278]),
+        (["818181911112111"], [888911112111]),
+    ],
+)
+def test_part2_get_largest_k_digit(num, expected):
+    out = d03.part2_get_largest_k_digit(num, k=12)
+
+    assert out == expected
+
+
+@pytest.mark.parametrize(
+    "num,expected",
+    [
+        (["987654321111111"], [987654321111]),
+        (["811111111111119"], [811111111119]),
+        (["234234234234278"], [434234234278]),
+        (["818181911112111"], [888911112111]),
+    ],
+)
+def test_part2_get_largest_k_digit_optimised(num, expected):
+    out = d03.part2_get_largest_k_digit_optimised(num, k=12)
+
+    assert out == expected
